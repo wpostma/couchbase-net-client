4aa57620f6a33e1cf87641c6c582ffac40ede849
Warren Postma <warren.postma@gmail.com>
2015-08-04 13:49:54 -0400

    Unit test Test_Get_Date added to GetOperationTests.
    Proves simple invariance of ISO date values when they are persisted and retrieved, either as the entire document,
    or as an attribute in a JSON Dictionary.
    
    See forums:
    https://forums.couchbase.com/t/surprising-transformations-on-iso-8601-values-unique-to-net-sdk/4678/12

--- Src/Couchbase.Tests/IO/Operations/GetOperationTests.cs
+++ Src/Couchbase.Tests/IO/Operations/GetOperationTests.cs
@@ -1,6 +1,9 @@
 ﻿using Couchbase.IO.Operations;
 using NUnit.Framework;
 
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
 namespace Couchbase.Tests.IO.Operations
 {
     [TestFixture]
@@ -17,6 +20,22 @@ namespace Couchbase.Tests.IO.Operations
             {
                 bucket.Upsert(TestKeys.KeyWithInt32Value.Key, TestKeys.KeyWithInt32Value.Value);
                 bucket.Upsert(TestKeys.KeyWithStringValue.Key, TestKeys.KeyWithStringValue.Value);
+
+                // ISO-8601 and Microsoft JSON Date format:
+                bucket.Upsert(TestKeys.KeyWithIsoDateValue.Key, TestKeys.KeyWithIsoDateValue.Value);
+                bucket.Upsert(TestKeys.KeyWithIsoUtcDateValue.Key, TestKeys.KeyWithIsoUtcDateValue.Value);
+                bucket.Upsert(TestKeys.KeyWithMsDateValue.Key, TestKeys.KeyWithMsDateValue.Value);
+
+                // De-Serialize const JSON string into a Dictionary<string, dynamic> and write via bucket.Upsert(Couchbase.Document<dynamic>)
+                var ADict = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(TestKeys.KeyWithJsonDocValueStr);
+                var couchDoc = new Couchbase.Document<dynamic>()
+                {
+                    Id = TestKeys.KeyWithJsonDocKey,
+                    Content = ADict
+                };
+                bucket.Upsert(couchDoc);
+
+
             }
         }
 
@@ -41,6 +60,48 @@ namespace Couchbase.Tests.IO.Operations
                 Assert.AreEqual(TestKeys.KeyWithInt32Value.Value, response.Value);
             }
         }
+
+        [Test]
+        public void Test_Get_Date()
+        {
+            using (var bucket = _cluster.OpenBucket("default"))
+            {
+                //Pass!
+                var response = bucket.Get<string>(TestKeys.KeyWithIsoUtcDateValue.Key);
+                Assert.IsTrue(response.Success);
+                Assert.AreEqual(TestKeys.KeyWithIsoUtcDateValue.Value, response.Value);
+
+
+                // see: https://forums.couchbase.com/t/surprising-transformations-on-iso-8601-values-unique-to-net-sdk/4678/8
+                // It's probably NOT a good idea to expect a string round-trip identity check (stringvalue1==stringvalue2) to pass, but the values should be equivalent
+                // when converted to System.DateTime. JSON datetimes are a many-headed beast, and so we should only assert that which we expect to really be done.
+                var response3 = bucket.Get<dynamic>("KeyWithJsonDocValue"); //  "RAMSOFT.AUDIT.STUDYACCESS.00485ff354cd491a9044aa6143608fa5" ); // ?  
+                Newtonsoft.Json.Linq.JObject Container = response3.Value;
+
+                JsonSerializerSettings settings = new JsonSerializerSettings
+                {
+                    DateFormatHandling = DateFormatHandling.IsoDateFormat,
+                    DateTimeZoneHandling = DateTimeZoneHandling.Utc
+                };
+                System.DateTime AValue = (System.DateTime )Container[TestKeys.IsoDateKeyName]; // Convert JToken to System.DateTime, in a valid JSON style date format (including ISO-8601, maybe others?)
+                System.DateTime ARefValue = System.DateTime.Parse(TestKeys.IsoDateValueSample, null, System.Globalization.DateTimeStyles.RoundtripKind);  // Convert constant string in ISO-8601 format, to System.DateTime.
+                Assert.AreEqual( AValue, ARefValue);
+
+                // This test would fail as well if we used string check (string1==string2)
+                //When you write an ISO-8601 value that does not END in an explicit timezone (like Z), it could automatically assume that it is UTC and add the Z.
+                var response2 = bucket.Get<string>(TestKeys.KeyWithIsoDateValue.Key);
+                Assert.IsTrue(response2.Success);
+                System.DateTime AValue2 = System.DateTime.Parse(response2.Value, null, System.Globalization.DateTimeStyles.RoundtripKind);  // Convert string in response.Value, assumed to be in ISO-8601 format, to System.DateTime.
+                System.DateTime ARefValue2 = System.DateTime.Parse(TestKeys.KeyWithIsoDateValue.Value, null, System.Globalization.DateTimeStyles.RoundtripKind);  // Convert constant string in ISO-8601 format, to System.DateTime.
+                Assert.AreEqual(AValue2, ARefValue2); // It should not surprise anyone who works a while with DateTime values in JSON that some effort is required to compare Values.
+
+
+
+
+            }
+        }
+
+
 
         [Test]
         public void When_Get_Follows_Set_Operation_Is_Correct()
--- Src/Couchbase.Tests/TestKeys.cs
+++ Src/Couchbase.Tests/TestKeys.cs
@@ -4,17 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 namespace Couchbase.Tests
 {
     public static class TestKeys
     {
+        public const string IsoDateValueSample = "2014-11-13T18:15:23";
+        public const string IsoDateKeyName = "isodatetime";
+
+        public const string KeyWithJsonDocKey = "KeyWithJsonDocValue";
+        public const string KeyWithJsonDocValueStr = @"{
+  ""uniqueidentifier"": ""1.2.124.113532.19.29269.37783.20120128.72021.7570795"",
+  ""isodatetime"": ""2014-11-13T18:15:23"",
+  ""shortstring"": ""ADMIN"",
+  ""mediumstring"": ""SOME MULTI WORD STRING"",
+  ""statusinteger"": 123456789,
+  ""digitsinquotes"": ""000408703"",
+  ""md5hash"": ""6D417FA9C6A430CD3ED34B69C57361DC""
+}";
         static TestKeys()
         {
             KeyWithStringValue = new KeyValuePair<string, string>("Key_With_String_Value", "string value.");
             KeyWithInt32Value = new KeyValuePair<string, int?>("Key_With_Int32_Value", 5242010);
+            KeyWithIsoDateValue = new KeyValuePair<string, string>("KeyWithIsoDateValue", "2014-11-28T11:01:18" );
+            KeyWithIsoUtcDateValue = new KeyValuePair<string, string>("KeyWithIsoDateValue", "2014-11-28T11:01:18Z" );
+            KeyWithMsDateValue = new KeyValuePair<string, string>("KeyWithMsDateValue", "2014-11-28 11:01:18" );
+
+            //KeyWithJsonDocValue = new KeyValuePair<string,string>("KeyWithJsonDocValue", KeyWithJsonDocValueStr );
+            //KeyWithJsonDocValue = new KeyValuePair<string, dynamic>("KeyWithJsonDocValue", JsonConvert.DeserializeObject(KeyWithJsonDocValueStr) );
+
         }
         public static KeyValuePair<string, string> KeyWithStringValue;
         public static KeyValuePair<string, int?> KeyWithInt32Value;
+
+        // ISO-8601 in JSON datetime
+        public static KeyValuePair<string, string> KeyWithIsoDateValue;
+        public static KeyValuePair<string, string> KeyWithIsoUtcDateValue;
+        public static KeyValuePair<string, string> KeyWithMsDateValue;
+    
+        // Complex JSON document
+       //public static KeyValuePair<string, dynamic> KeyWithJsonDocValue;
+
     }
 }
 
